1c839aa26857638693e5598511140d3c6fcf46f8
rubylo718 <rubylo718@gmail.com>
2023-09-26 15:30:52 +0800

    feat: add text while empty cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -8,70 +8,82 @@ const Cart = () => {
 		<div className="table-responsive bg-light p-3">
 			<table className="table align-middle">
 				<tbody>
-					{state.cartList.map((item) => {
-						return (
-							<tr key={item.id}>
-								<td>
-									<button
-										type="button"
-										className="btn btn-sm"
-										onClick={() => {
-											dispatch({
-												type: 'REMOVE_CART_ITEM',
-												payload: {
-													...item,
-												},
-											})
-										}}
-									>
-										x
-									</button>
-								</td>
-								<td>
-									<img className="table-img" src={item.img} alt={item.title} />
-								</td>
-								<td>
-									{item.title}
-									<br />
-									<small className="text-muted">NT$ {item.price}</small>
-								</td>
-								<td>
-									<select
-										className="form-select"
-										value={item.quantity}
-										onChange={(e) => {
-											e.preventDefault()
-											const quantity = parseInt(e.target.value)
-											dispatch({
-												type: 'CHANGE_CART_QTY',
-												payload: {
-													...item,
-													quantity,
-												},
-											})
-										}}
-									>
-										{[...Array(20)].map((_, i) => {
-											return (
-												<option value={i + 1} key={i}>
-													{i + 1}
-												</option>
-											)
-										})}
-									</select>
-								</td>
-								<td className="text-end">NT$ {item.price * item.quantity}</td>
-							</tr>
-						)
-					})}
+					{state?.cartList?.length > 0 ? (
+						state.cartList.map((item) => {
+							return (
+								<tr key={item.id}>
+									<td>
+										<button
+											type="button"
+											className="btn btn-sm"
+											onClick={() => {
+												dispatch({
+													type: 'REMOVE_CART_ITEM',
+													payload: {
+														...item,
+													},
+												})
+											}}
+										>
+											x
+										</button>
+									</td>
+									<td>
+										<img
+											className="table-img"
+											src={item.img}
+											alt={item.title}
+										/>
+									</td>
+									<td>
+										{item.title}
+										<br />
+										<small className="text-muted">NT$ {item.price}</small>
+									</td>
+									<td>
+										<select
+											className="form-select"
+											value={item.quantity}
+											onChange={(e) => {
+												e.preventDefault()
+												const quantity = parseInt(e.target.value)
+												dispatch({
+													type: 'CHANGE_CART_QTY',
+													payload: {
+														...item,
+														quantity,
+													},
+												})
+											}}
+										>
+											{[...Array(20)].map((_, i) => {
+												return (
+													<option value={i + 1} key={i}>
+														{i + 1}
+													</option>
+												)
+											})}
+										</select>
+									</td>
+									<td className="text-end">NT$ {item.price * item.quantity}</td>
+								</tr>
+							)
+						})
+					) : (
+						<tr>
+							<td className="text-center bg-light">購物車內無品項</td>
+						</tr>
+					)}
 				</tbody>
-				<tfoot>
-					<tr>
-						<td colSpan={5} className="text-end">
-							總金額 NT$ {state.total || 0}
-						</td>
-					</tr>
-				</tfoot>
+				{state?.cartList?.length > 0 && (
+					<tfoot>
+						<tr>
+							<td colSpan={5} className="text-end">
+								總金額 NT$ {state.total || 0}
+							</td>
+						</tr>
+					</tfoot>
+				)}
 			</table>
 		</div>
 	)
