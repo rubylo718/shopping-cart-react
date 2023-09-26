a3e66808d86e5783dfaa60a977e064e437900d79
rubylo718 <rubylo718@gmail.com>
2023-09-26 13:14:06 +0800

    fix: refactor to map Prodects outside the component to own add number state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Products from './components/Products'
 import Cart from './components/Cart'
 import { CartProvider } from './contexts/CartContext'
 import './styles/style.scss'
+import productsData from './assets/productsData'
 
 function App() {
 	return (
@@ -11,7 +12,18 @@ function App() {
 			<div className="container mt-4">
 				<div className="row">
 					<div className="col-md-7">
-						<Products />
+						<div className="row row-cols-3 g-3">
+							{productsData.map((product) => {
+								return (
+									<Products
+										id={product.id}
+										title={product.title}
+										imgPath={product.img}
+										price={product.price}
+									/>
+								)
+							})}
+						</div>
 					</div>
 					<div className="col-md-5">
 						<Cart />
@@ -21,6 +33,5 @@ function App() {
 		</CartProvider>
 	)
 }
-
 
 export default App
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,67 +1,56 @@
 import { useState, useContext } from 'react'
-import productsData from '../assets/productsData'
 import { CartDispatchContext } from '../contexts/CartContext'
 
-const Products = () => {
+const Products = ({ id, title, imgPath, price }) => {
 	const dispatch = useContext(CartDispatchContext)
 	const [addQty, setAddQty] = useState(1)
 	return (
-		<div className="row row-cols-3 g-3">
-			{productsData.map((product) => {
-				return (
-					<div className="col" key={product.id}>
-						<div className="card">
-							<img
-								src={product.img}
-								className="card-img-top"
-								alt="{product.title}"
-							/>
-							<div className="card-body">
-								<h6 className="card-title">
-									{product.title}
-									<span className="float-end">NT$ {product.price}</span>
-								</h6>
-								<div
-									className="btn-group btn-group-sm d-flex align-items-center mb-2"
-									role="group"
-								>
-									<button
-										type="button"
-										className="btn btn-primary w-25"
-										onClick={() => {
-											addQty > 1 && setAddQty(addQty - 1)
-										}}
-									>
-										-
-									</button>
-									<span className="text-center w-50">{addQty}</span>
-									<button
-										type="button"
-										className="btn btn-primary p-1 w-25"
-										onClick={() => {
-											setAddQty(addQty + 1)
-										}}
-									>
-										+
-									</button>
-								</div>
-								<button
-									type="button"
-									className="btn btn-outline-primary w-100"
-									onClick={() => {
-										dispatch({
-											type: 'ADD_TO_CART',
-											payload: { ...product, quantity: 1 },
-										})
-									}}
-								>
-									加入購物車
-								</button>
-							</div>
-						</div>
+		<div className="col" key={id}>
+			<div className="card">
+				<img src={imgPath} className="card-img-top" alt={title} />
+				<div className="card-body">
+					<h6 className="card-title">
+						{title}
+						<span className="float-end">NT$ {price}</span>
+					</h6>
+					<div
+						className="btn-group btn-group-sm d-flex align-items-center mb-2"
+						role="group"
+					>
+						<button
+							type="button"
+							className="btn btn-primary w-25"
+							onClick={() => {
+								addQty > 1 && setAddQty(addQty - 1)
+							}}
+						>
+							-
+						</button>
+						<span className="text-center w-50">{addQty}</span>
+						<button
+							type="button"
+							className="btn btn-primary p-1 w-25"
+							onClick={() => {
+								setAddQty(addQty + 1)
+							}}
+						>
+							+
+						</button>
 					</div>
-				)
-			})}
+					<button
+						type="button"
+						className="btn btn-outline-primary w-100"
+						onClick={() => {
+							dispatch({
+								type: 'ADD_TO_CART',
+								payload: { id, title, img: imgPath, price, quantity: addQty },
+							})
+						}}
+					>
+						加入購物車
+					</button>
+				</div>
+			</div>
 		</div>
 	)
 }
