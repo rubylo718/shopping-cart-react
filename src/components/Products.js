072e541943fb888f99a1b9341dfbc21ef1b3aa7e
rubylo718 <rubylo718@gmail.com>
2023-09-26 13:03:42 +0800

    feat: add product number in cards

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,10 @@
-import { useContext } from 'react'
+import { useState, useContext } from 'react'
 import productsData from '../assets/productsData'
 import { CartDispatchContext } from '../contexts/CartContext'
 
 const Products = () => {
 	const dispatch = useContext(CartDispatchContext)
+	const [addQty, setAddQty] = useState(1)
 	return (
 		<div className="row row-cols-3 g-3">
 			{productsData.map((product) => {
@@ -20,6 +21,30 @@ const Products = () => {
 									{product.title}
 									<span className="float-end">NT$ {product.price}</span>
 								</h6>
+								<div
+									className="btn-group btn-group-sm d-flex align-items-center mb-2"
+									role="group"
+								>
+									<button
+										type="button"
+										className="btn btn-primary w-25"
+										onClick={() => {
+											addQty > 1 && setAddQty(addQty - 1)
+										}}
+									>
+										-
+									</button>
+									<span className="text-center w-50">{addQty}</span>
+									<button
+										type="button"
+										className="btn btn-primary p-1 w-25"
+										onClick={() => {
+											setAddQty(addQty + 1)
+										}}
+									>
+										+
+									</button>
+								</div>
 								<button
 									type="button"
 									className="btn btn-outline-primary w-100"
